ac20e625e8b5714cbece9916ef449a35f8608a42
Unknown <ikitess@gmail.com>
2018-04-23 14:27:12 +0200

    build pre-menu 60 fps

---
+++ flappyBird/Assets/Scripts/Gato.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gato : MonoBehaviour
+{
+	public static Gato instancia;
+
+	public Rigidbody2D cuerpo2D;
+	public Animator animador;
+	public float tiempoAnim = 0.5f;
+	public AudioSource auidoVolar;
+
+	public float tapForce = 200;
+	public float tiltSmooth = 0.1f;
+	public Vector3 startPos;
+
+	Quaternion downRotation;
+	Quaternion forwardRotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (instancia == null) {
+			instancia = this;
+		}
+		cuerpo2D = GetComponent<Rigidbody2D> ();
+		downRotation = Quaternion.Euler (0, 0, -35);
+		forwardRotation = Quaternion.Euler (0, 0, 35);
+
+		animador = GetComponent<Animator> ();
+		animador.SetTrigger ("Normal");
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//volver a poner el gato en animacion normal
+		tiempoAnim -= Time.fixedDeltaTime;
+		if (tiempoAnim < 0) { 
+			animador.SetTrigger ("Normal");
+			tiempoAnim = -1f;
+		}
+	}
+
+	//tap para que suba
+	public void gatoVolar ()
+	{		
+		cuerpo2D.velocity = Vector3.zero;
+		transform.rotation = forwardRotation;
+		cuerpo2D.AddForce (Vector2.up * tapForce, ForceMode2D.Force);
+		animador.SetTrigger ("Impulso");
+		auidoVolar.Play ();
+	}
+
+	public void gatoCaidaSuave() {
+		//parezca que suba y caiga de forma suave
+		transform.rotation = Quaternion.Lerp (transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+	}
+
+
+
+}
--- flappyBird/Assets/controladorJuego.cs
+++ flappyBird/Assets/controladorJuego.cs
@@ -2,41 +2,109 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class controladorJuego : MonoBehaviour {
+public class controladorJuego : MonoBehaviour
+{
 
+	//intancia de la propia clase
 	public static controladorJuego instancia;
-    public Rigidbody2D gato;
-    public GameObject controladorNube;
-    public GameObject controladorColumna;
 
+	//controles varios
+	public Rigidbody2D gato;
+	public GameObject controladorNube;
+	public GameObject controladorColumna;
 
+	//control del gato
+	public GameObject controlGato;
 
+	//UI para el score y la muerte
+	public Text inicioText;
 	public Text scoreTexto;
+	public Text gameOverText;
+	public Text reinicioText;
 	float score = 0;
+
+	//juego activo o no
+	public bool juegoActivo;
+	public bool gatoMuerto;
+	bool creadoraColumnas = false;
+	bool creadoraNubes = true;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		juegoActivo = false;
 		if (instancia == null) {
 			instancia = this;
 		}
-		scoreTexto.text = "asdf";
+		scoreTexto.text = "Score: " + score;
+		gameOverText.enabled = false;
+		reinicioText.enabled = false;
 	}
+
+	//poner a 60 fps el juego
+	void Awake ()
+	{
+		Application.targetFrameRate = 60;
+	}
+
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// se controle todo el juego desde aqui
+	void Update ()
+	{
+		//control de si el juego esta iniciado o hay que iniciarlo
+		if (!juegoActivo && !gatoMuerto) {
+			gato.bodyType = RigidbodyType2D.Static;
+			//de juego inactivo a activo
+			if (Input.GetMouseButtonDown (0)) {
+				juegoActivo = true;
+				gato.bodyType = RigidbodyType2D.Dynamic;
+				Gato.instancia.gatoVolar ();
+				creadoraColumnas = true;
+				inicioText.enabled = false;
+			}
+
+		} else if (juegoActivo) {
+			Gato.instancia.gatoCaidaSuave ();
+			//juego activo y hacen tap
+			if (Input.GetMouseButtonDown (0)) {
+				gato.bodyType = RigidbodyType2D.Dynamic;
+				Gato.instancia.gatoVolar ();
+				creadoraColumnas = true;
+			}
+
+		}
+
+		//update de columnas
+		if (creadoraColumnas) {
+			creadoraDeColumnas.instancia.creadoraColumnas ();
+		}
+
+		//gato muerto, reiniciar
+		if (gatoMuerto) {
+			//recargar
+			if (Input.GetMouseButtonDown (0)) {
+				SceneManager.LoadScene (0);
+			}
+		}
 
 	}
 
-	public void scored(){
-		score +=0.5f;
+	public void scored ()
+	{
+		score += 0.5f;
 		scoreTexto.text = "Score: " + score;
 	}
 
-    public void muerte() {
-        creadoraDeColumnas.instancia.pararColumnas();
-        pararSuelos.instancia.parar();
-        //gato.velocity = Vector3.zero;
-        scoreTexto.text = "Muerto"; 
-    }
+	public void muerte ()
+	{
+		juegoActivo = false;
+		gatoMuerto = true;
+		creadoraDeColumnas.instancia.pararColumnas ();
+		pararSuelos.instancia.parar ();
+		//gato.velocity = Vector3.zero;
+		gameOverText.enabled = true;
+		reinicioText.enabled = true;
+	}
 }
