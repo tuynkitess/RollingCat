ea54cf5a71ed2d1a674484ea99b86ac62b9faa5e
Unknown <inaki.caballero@egibide.org>
2018-04-26 21:59:53 +0200

    funciona

---
+++ flappyBird/Assets/controladorBaseDatos.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class controladorBaseDatos : MonoBehaviour
+{
+
+
+
+
+
+	public void subirPuntuacion (int score)
+	{
+	
+	}
+
+	public void consultarTuPuntuacion ()
+	{
+	}
+
+	public void consultarPuntuacionesGlobales ()
+	{
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
